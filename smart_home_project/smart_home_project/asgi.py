13a87f69689613c02bfc854ce9e94e107511bae3
leonidgerasim <porovozov.987654321@mail.ru>
2025-08-12 19:24:34 +0300

    Release 1.1

--- smart_home_project/smart_home_project/asgi.py
+++ smart_home_project/smart_home_project/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from . import routing  # Импортируем routing из текущей директории
+from . import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_home_project.settings')
